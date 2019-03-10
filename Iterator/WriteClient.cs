f3760c50c46b721628c77149276c6a66f465ae9b
Symasoiti97 <symasoiti97@gmail.com>
2019-03-10 13:58:36 +0200

    add Class: WriteClient

---
+++ Iterator/WriteClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterators
+{
+    class WriteClient
+    {
+        Client client;
+
+        public WriteClient(Client client)
+        {
+            this.client = client;
+        }
+
+        public void PrintClient()
+        {
+            
+        }
+    }
+}
